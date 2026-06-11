9fe85d0bf6535d21a24cc1aac952f479ef89dbee
Barnali-M <malakar.barnali120@gmail.com>
2026-06-11 08:33:12 +0530

    Update Disk_space_monitor.sh

--- Disk_space_monitor.sh
+++ Disk_space_monitor.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Check disk usage.
-
-# If usage exceeds 80%:
-
-# WARNING: Disk usage is 85%
-
 usage=$(df -h | awk 'NR==2 { gsub("%"," "$5); print $5'})
 
+# If usage exceeds 80%:
 if [[ "$usage" -gt 80 ]];then
+
+    # WARNING: Disk usage is 85%
     echo "Warning: Disk usage is ${user}%"
+    
 else   
     echo "Disk usage is normal ${user}%"
 fi
